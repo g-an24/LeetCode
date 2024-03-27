b4f620edf56d9bb24e98311310c5e5ff1092fe54
Anany Gangver <57257255+g-an24@users.noreply.github.com>
2024-03-26 23:56:01 -0400

    Time: 5 ms (53.16%), Space: 42.7 MB (93.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] sCounter = new int[26];
+        int[] tCounter = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+            sCounter[s.charAt(i) - 'a']++;
+            tCounter[t.charAt(i) - 'a']++;
+        }
+        
+        for(int i=0; i<26; i++){
+            if(sCounter[i] != tCounter[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
