3f9cb75df8e4ae12ae4157c2c800d145117f6f9a
Anany Gangver <57257255+g-an24@users.noreply.github.com>
2024-03-26 23:29:14 -0400

    Time: 18 ms (33.29%), Space: 55 MB (90.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+
+        for(int i=0; i<n-1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+
+        }
+        return false;
+        
+    }
+}
