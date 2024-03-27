1534f4bd9e447c6412e63c318d1fbdf1a03ff9bd
Anany Gangver <57257255+g-an24@users.noreply.github.com>
2024-03-27 00:08:40 -0400

    Time: 1 ms (96.36%), Space: 45.3 MB (43.88%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,15 +1,26 @@
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int n = nums.length;
+        
+//         int[] num_new = new int[2*n];
+        
+//         for(int i=nums.length; i<num_new.length; i++){
+//             num_new[i-nums.length] = nums[i-nums.length];
+//             num_new[i] = nums[i-nums.length];
+//         }
+        
+//         return num_new;
+        
+//     }
+// }
+
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        
-        int[] num_new = new int[2*n];
-        
-        for(int i=nums.length; i<num_new.length; i++){
-            num_new[i-nums.length] = nums[i-nums.length];
-            num_new[i] = nums[i-nums.length];
+        int[] ans = new int[nums.length*2];
+        for(int i=0;i<nums.length;i++) {
+            ans[i] = nums[i];
+            ans[i+nums.length] = nums[i];
         }
-        
-        return num_new;
-        
+        return ans;
     }
 }
